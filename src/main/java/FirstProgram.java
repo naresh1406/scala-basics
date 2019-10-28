8557dd232239c14f612f4fdd9f9987221b08e0e4
naresh1406 <nareshgupta1406@egmail.com>
2019-10-28 17:54:49 +0700

    Hello Wolrd

---
+++ src/main/java/FirstProgram.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ngupta on 28/10/2019 AD.
+ */
+class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("First Program");
+    }
+}
---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,6 @@
+/**
+ * Created by ngupta on 28/10/2019 AD.
+ */
+object HelloWorld extends App {
+    println("Hello World!!!")
+}
