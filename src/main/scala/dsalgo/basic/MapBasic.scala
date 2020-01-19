35ca2e3ca6eb04c65e698612adb131b7f24e22e8
naresh1406 <nareshgupta1406@egmail.com>
2020-01-19 14:56:56 +0700

    map basic update

--- src/main/scala/dsalgo/basic/MapBasic.scala
+++ src/main/scala/dsalgo/basic/MapBasic.scala
@@ -4,7 +4,12 @@ package dsalgo.basic
  * Created by ngupta on 31/10/2019 AD.
  */
 object MapBasic extends App {
-  var map = Map(1 -> 'a', 2 -> 'b', 3 -> 'c')
+  val map = Map(1 -> 'a', 2 -> 'b', 3 -> 'c') //by default scala Map is immutable
   println(map)
+
+  val mutableMap = scala.collection.mutable.Map(1 -> "I", 2 -> "II", 3 -> "III", 4 -> "IV", 5 -> "V")
+  println(mutableMap)
+  mutableMap(1) = "update"
+  println(mutableMap)
 
 }
